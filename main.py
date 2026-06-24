66a65e9b007e4031f7d13bd67312bed29255d75f
Patrick Odenwald <odenwald.patrick@gmail.com>
2026-06-24 23:04:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from src.scraper import download
+from src.parser import parse
+from src.calendar_generator import generate
 
+html = download()
+
+events = parse(html)
+
+calendar = generate(events)
+
+with open("calendar.ics","wb") as f:
+    f.write(calendar.to_ical())
