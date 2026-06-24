0789b9bb3c0a228411c5f160842a4244d69aa634
Patrick Odenwald <odenwald.patrick@gmail.com>
2026-06-24 23:03:33 +0200

    Update scraper.py

--- src/scraper.py
+++ src/scraper.py
@@ -1,1 +1,19 @@
+import requests
 
+URL = "https://wannkommtwas.de"
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+def download():
+
+    response = requests.get(
+        URL,
+        headers=HEADERS,
+        timeout=30
+    )
+
+    response.raise_for_status()
+
+    return response.text
